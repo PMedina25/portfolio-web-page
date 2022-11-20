bf7655145fa93b501a040c9b9ab3bec97d0e4645
Pablo <pablo.medina@the-cocktail.com>
2022-11-20 19:46:59 +0100

    feat(uikit): Set anchor tag styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,40 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: [
-    "./pages/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./pages/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px'
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
     colors: {
       ...defaultTheme.colors,
-      'primary': 'rgb(25, 118, 210)',
-      'secondary': 'rgb(55, 125, 255)',
-      'transparet': 'transparent',
-      'dark-blue': '#222B44',
-      'darker-blue': '#202841',
-      'white': '#ffffff',
-      'gray': 'rgb(174, 176, 180)',
+      primary: "rgb(25, 118, 210)",
+      secondary: "rgb(55, 125, 255)",
+      transparet: "transparent",
+      "dark-blue": "#222B44",
+      "darker-blue": "#202841",
+      white: "#ffffff",
+      gray: "rgb(174, 176, 180)",
     },
     spacing: {
-      '1': '8px',
-      '2': '12px',
-      '3': '16px',
-      '4': '24px',
-      '5': '32px',
-      '6': '40px',
-      '7': '48px',
+      1: "8px",
+      2: "12px",
+      3: "16px",
+      4: "24px",
+      5: "32px",
+      6: "40px",
+      7: "48px",
     },
     fontSize: {
       ...defaultTheme.fontSize,
-      'text-3xl': '2rem',
-    }
+      "text-3xl": "2rem",
+    },
+    fontFamily: {
+      inter: ["Inter"],
+    },
   },
 };
